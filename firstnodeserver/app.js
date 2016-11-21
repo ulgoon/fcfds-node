60e720145fa2b8926aff8f28e517de8d401999ab
ulgoon <ulgoon89@gmail.com>
2016-11-21 16:49:52 +0900

    detailId route

--- firstnodeserver/app.js
+++ firstnodeserver/app.js
@@ -7,7 +7,15 @@ var app = http.createServer(function(request, response){
     if (request.url === "/"){
         response.write("This is HOME!!!!!!");
         response.end();
+    }
+    
+    var detailId = request.url.replace("/", "");
+    if (detailId.length > 0){
+        response.write(detailId);
+        response.end();
     } 
+
+
 }).listen(process.env.PORT || 3030);
 
 console.log("First Node Server is running at localhost");
