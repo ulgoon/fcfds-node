88eb5eacc01c6188d8519aba0969accd0df2d10c
ulgoon <ulgoon89@gmail.com>
2016-11-21 16:30:13 +0900

    add home route: add home route in app.js print request.url at console

--- firstnodeserver/app.js
+++ firstnodeserver/app.js
@@ -1,10 +1,13 @@
 var http = require('http');
 
 
-http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write("Hello FDS!!!");
-    response.end();
+var app = http.createServer(function(request, response){
+    console.log(request.url);
+
+    if (request.url === "/"){
+        response.write("This is HOME!!!!!!");
+        response.end();
+    } 
 }).listen(process.env.PORT || 3030);
 
 console.log("First Node Server is running at localhost");
