5a117fa41ae11bcf246751ac89833c16a1ca322d
ulgoon <ulgoon89@gmail.com>
2016-11-23 14:05:06 +0900

    express server start + about routing with about.js, index.js

---
+++ secondexpress/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+var indexRouter = require("./routes/index")(app);
+var aboutRouter = require("./routes/about")(app);
+
+app.set("port", process.env.PORT || 3030);
+
+app.use(function(req, res){
+    res.type("text/plain");
+    res.status("404");
+    res.send("404 - Not found");
+});
+
+app.use(function(req, res){
+    res.type("text/plain");
+    res.status("500");
+    res.send("500 - Server error");
+});
+
+app.listen(app.get("port"), function(){
+    console.log("Second Express Server is running at localhost:" + app.get("port"));
+});
