8d2d5b5c8c2eb7a5ff9d228ad7f06f97f81179a4
ulgoon <ulgoon89@gmail.com>
2016-11-24 14:18:35 +0900

    delete chat router

--- secondexpress/server.js
+++ secondexpress/server.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var app = express();
 
-
-
-
 var indexRouter = require("./routes/index")(app);
 var aboutRouter = require("./routes/about")(app);
 var usersRouter = require("./routes/users")(app);
-var chatRouter = require("./routes/chat")(app);
 
 
 app.set("port", process.env.PORT || 3030);
