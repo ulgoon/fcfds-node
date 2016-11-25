a83dee5298579cf9c8f6762d9170d67a40215569
ulgoon <ulgoon89@gmail.com>
2016-11-25 14:32:33 +0900

    complete gulp-imagmin

--- gulpexpress/gulpfile.js
+++ gulpexpress/gulpfile.js
@@ -1,16 +1,17 @@
+// main module gulp, pump
 var gulp = require('gulp');
 var pump = require('pump');
-
+// gulp tasks
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-
-
+var imagemin = require('gulp-imagemin');
+// specify public paths
 var publicPath = {
     src : './public/src/',
     dest : './public/dist/'
 };
 
-
+// task hello
 gulp.task("hello", function(){
     return console.log("hello");
 });
@@ -32,6 +33,15 @@ gulp.task("concat", function(){
         gulp.src([publicPath.src + 'js/concat1.js', publicPath.src + 'js/concat3.js', publicPath.src + 'js/concat2.js']),
         concat('concatenated.js'),
         gulp.dest(publicPath.dest + 'js/')
+    ]);
+});
+
+// image minify
+gulp.task("imagemin", function(){
+    pump([
+        gulp.src([publicPath.src + 'img/*.jpg', publicPath.src + 'img/*.png']),
+        imagemin(),
+        gulp.dest(publicPath.dest + 'img/')
     ]);
 });
 
